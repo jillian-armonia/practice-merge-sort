d913c2325bcd3a264de948a0249a84299c1cf1e1
Jillian Armonia <jillian.marice@gmail.com>
2024-09-03 22:46:49 +0900

    finished out-of-place merge

--- merge-sort.js
+++ merge-sort.js
@@ -1,16 +1,39 @@
 // Merge Sort out-of-place
 // Do not modify the original array
 function mergeSort(arr) {
+  let merged = [];
 
-  // Check if the input is length 1 or less
+  if (arr.length <= 1) return arr; // Check if the input is length 1 or less
     // If so, it's already sorted: return
 
   // Divide the array in half
+  let halfIndex = Math.floor(arr.length / 2);
+  let left = arr.slice(0, halfIndex);
+  let right = arr.slice(halfIndex);
 
   // Recursively sort the left half
   // Recursively sort the right half
+  left = mergeSort(left);
+  right = mergeSort(right);
 
   // Merge the halves together and return
+  let j = 0;
+  let i = 0;
+
+  while (i < left.length || j < right.length){
+    if (right[j] === undefined || left[i] < right[j]){
+      merged.push(left[i]);
+      i++;
+    }
+
+    if (left[i] === undefined || left[i] > right[j]){
+      merged.push(right[j]);
+      j++;
+    }
+
+  }
+
+  return merged;
 
 }
 
@@ -31,4 +54,3 @@ function merge(arrA, arrB) {
 }
 
 module.exports = [merge, mergeSort];
-
